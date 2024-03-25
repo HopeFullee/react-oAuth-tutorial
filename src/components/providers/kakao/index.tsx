3bfee84840c1df0567c2f4c943f1c0edbd6d1677
hope <john_lee0607@naver.com>
2024-03-25 13:27:15 +0900

    refactor: kakao 로그인 리펙토링

--- src/components/providers/kakao/index.tsx
+++ src/components/providers/kakao/index.tsx
@@ -1,6 +1,7 @@
 import { SetStateAction, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import axios from 'axios';
+import qs from 'qs';
 
 interface Props {
   setSession: React.Dispatch<SetStateAction<{ provider: string; authId: string }>>;
@@ -9,50 +10,55 @@ interface Props {
 export const KakaoAuth = ({ setSession }: Props) => {
   const [searchParams] = useSearchParams();
 
-  const REST_API_KEY = `${process.env.REACT_APP_KAKAO_REST_KEY ?? ''}`;
-  const REDIRECT_URI = `${process.env.REACT_APP_REDIRECT_URL}`;
+  const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_KEY ?? '';
+  const CLIENT_SECRET = process.env.REACT_APP_KAKAO_SECRET ?? '';
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const loginWithKakao = async (kakaoAuthCode: string) => {
+    try {
+      const uri = 'https://kauth.kakao.com/oauth/token';
+      const body = qs.stringify({
+        grant_type: 'authorization_code',
+        client_id: REST_API_KEY,
+        client_secret: CLIENT_SECRET,
+        redirect_uri: REDIRECT_URI,
+        code: kakaoAuthCode,
+      });
+      const headers = { 'Content-type': 'application/x-www-form-urlencoded' };
+
+      // 발급받은 kakaoAuthCode 로 토큰 요청
+      const tokenRes = await axios.post(uri, body, { headers: headers });
+
+      // 토큰을 성공적으로 발급 받았다면 실행
+      if (tokenRes.status === 200) {
+        const { access_token } = tokenRes.data;
+        const uri = 'https://kapi.kakao.com/v2/user/me';
+        const headers = {
+          Authorization: `Bearer ${access_token}`,
+          'Content-type': 'application/x-www-form-urlencoded',
+        };
+
+        // 발급받은 토큰으로 회원정보 요청 (예: 닉네임, 회원 고유 ID 등등..)
+        const userInfo = await axios.post(uri, {}, { headers: headers });
+
+        // 요청한 회원정보로 로그인 핸들링
+        return userInfo;
+      }
+    } catch (err) {
+      console.log('로그인 실패', err);
+    }
+  };
 
   useEffect(() => {
     // redirect 이후 kakao 에서 code 받아옴
     const kakaoAuthCode = searchParams.get('code');
     if (!kakaoAuthCode) return;
 
-    const grantType = 'authorization_code';
-
-    // 발급받은 code 로 access 토큰 요청
-    axios
-      .post(
-        `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${kakaoAuthCode}`,
-        {},
-        { headers: { 'Content-type': 'application/x-www-form-urlencoded;charset=utf-8' } }
-      )
-      .then((res: any) => {
-        const { access_token } = res.data;
-
-        // 발급받은 access 토큰으로 회원정보 요청 (예: 닉네임, 회원 고유 ID 등등..)
-        axios
-          .post(
-            `https://kapi.kakao.com/v2/user/me`,
-            {},
-            {
-              headers: {
-                Authorization: `Bearer ${access_token}`,
-                'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
-              },
-            }
-          )
-          .then((res: any) => {
-            console.log('카카오 로그인', res);
-            setSession({
-              provider: 'Kakao',
-              authId: res.data.id,
-            });
-          });
-      })
-      .catch((Error: any) => {
-        console.log(Error);
-      });
+    loginWithKakao(kakaoAuthCode).then((userInfo) => {
+      // 로그인 핸들링
+      console.log('use effect', userInfo);
+    });
   }, []);
 
   return (
